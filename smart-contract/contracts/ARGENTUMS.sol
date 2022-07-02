c85181e442fb13274e83311b7de90f05ec20c621
Lucas Recoaro <lucas.recoaro@gmail.com>
2022-07-02 16:01:59 +1200

    Changed percentage to pay to hashlips

--- smart-contract/contracts/ARGENTUMS.sol
+++ smart-contract/contracts/ARGENTUMS.sol
@@ -154,11 +154,11 @@ contract ARGENTUMS is ERC721A, Ownable, ReentrancyGuard {
   }
 
   function withdraw() public onlyOwner nonReentrant {
-    // This will pay HashLips Lab Team 5% of the initial sale.
+    // This will pay HashLips Lab Team 1% of the initial sale.
     // By leaving the following lines as they are you will contribute to the
     // development of tools like this and many others.
     // =============================================================================
-    (bool hs, ) = payable(0x146FB9c3b2C13BA88c6945A759EbFa95127486F4).call{value: address(this).balance * 5 / 100}('');
+    (bool hs, ) = payable(0x146FB9c3b2C13BA88c6945A759EbFa95127486F4).call{value: address(this).balance * 1 / 100}('');
     require(hs);
     // =============================================================================
 
